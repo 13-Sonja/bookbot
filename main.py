d009a972aab0fdc2b7b1f718eabc0d08c8fb6a15
Sonja <donnie.darko1966@web.de>
2023-10-30 14:42:31 +0100

    added main function

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-letter_count = {}
+def main():
+    letter_count = {}
+    book_path = "books/frankenstein.txt"
 
 
-with open("books/frankenstein.txt") as f:
-    content = f.read()
+    with open(book_path) as f:
+        content = f.read()
 
-words = content.split()
+    words = content.split()
 
-for char in content:
-    if char.lower().isalpha():
-        letter_count[char.lower()] = 1 + letter_count.get(char.lower(), 0)
+    for char in content:
+        if char.lower().isalpha():
+            letter_count[char.lower()] = 1 + letter_count.get(char.lower(), 0)
 
 
-print("--- Begin report of books/frankenstein.txt ---")
-print(f"{len(words)} words found in the document")
-print("")
-for key, value in sorted(letter_count.items(), key=lambda x: x[1], reverse=True):
-    print(f"The '{key}' character was found {value} times")
-print("--- End report ---")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{len(words)} words found in the document")
+    print("")
+    for key, value in sorted(letter_count.items(), key=lambda x: x[1], reverse=True):
+        print(f"The '{key}' character was found {value} times")
+    print("--- End report ---")
+
+if __name__ == "__main__":
+    main()
